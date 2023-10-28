411250ca44c5251167a3d05d9ef57debb721d2c7
Chris Gambrell <waymer147@gmail.com>
2023-10-28 19:54:10 -0400

    fix compilation error

--- Assets/CameraFollow.cs
+++ Assets/CameraFollow.cs
@@ -19,12 +19,12 @@ public class CameraFollow : MonoBehaviour
 	void FixedUpdate () {
 		if (target)
 		{
-			// Vector3 posNoZ = transform.position;
-			// posNoZ.z = target.transform.position.z;
+			Vector3 posNoZ = transform.position;
+			posNoZ.z = target.transform.position.z;
 
-			// Vector3 targetDirection = (target.transform.position - posNoZ);
+			Vector3 targetDirection = (target.transform.position - posNoZ);
 
-			// interpVelocity = targetDirection.magnitude * 5f;
+			interpVelocity = targetDirection.magnitude * 5f;
 
 			targetPos = transform.position + (targetDirection.normalized * interpVelocity * Time.deltaTime); 
 			transform.position = Vector3.Lerp( transform.position, targetPos + offset, 0.25f);
